63ca0782e833103ccacef7a116e7cb6293f2efb5
nikalinov <nikitakalinovskij@gmail.com>
2023-02-18 20:56:29 +0000

    Facebook challenge

---
+++ SAS/Facebook.py
@@ -0,0 +1,39 @@
+
+users = {}
+
+# Create a dictionary {user: [friend_1, friend_2, ...]}
+with open("friends05.txt") as f:
+    for line in f:
+        user, friend = line.rstrip("\n").split(" ")
+        if user in users:
+            users[user].append(friend)
+        else:
+            users[user] = [friend]
+
+groups = []
+print(users)
+# Test
+
+for user in users:
+    groups.append([user])
+    for friend in users[user]:
+        if friend not in users:
+            continue
+        if user not in users[friend]:
+            continue
+        if len(groups[-1]) >= 2:
+            for friend_prev in groups[-1][1:]:
+                if friend in users[friend_prev] and friend_prev in users[friend]:
+                    continue
+                break
+            else:
+                groups[-1].append(friend)
+        else:
+            groups[-1].append(friend)
+
+for group in sorted(groups, key=lambda x: len(x)):
+    print(group)
+
+
+
+
