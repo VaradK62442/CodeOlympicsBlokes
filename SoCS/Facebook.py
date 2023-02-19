977c944e6e4532804123b2d650aa4f35ef20b6a8
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2023-02-19 00:43:18 +0000

    facebook renaming

--- SoCS/Facebook.py
+++ SoCS/Facebook.py
@@ -1,39 +1,49 @@
-
-users = {}
-
-# Create a dictionary {user: [friend_1, friend_2, ...]}
-with open("friends05.txt") as f:
-    for line in f:
-        user, friend = line.rstrip("\n").split(" ")
-        if user in users:
-            users[user].append(friend)
-        else:
-            users[user] = [friend]
-
-groups = []
-print(users)
-# Test
-
-for user in users:
-    groups.append([user])
-    for friend in users[user]:
-        if friend not in users:
-            continue
-        if user not in users[friend]:
-            continue
-        if len(groups[-1]) >= 2:
-            for friend_prev in groups[-1][1:]:
-                if friend in users[friend_prev] and friend_prev in users[friend]:
-                    continue
-                break
+def read_in_file(fname):
+    users = {}
+    # create a dictionary {user: [friend_1, friend_2, ...]}
+    with open(fname) as f:
+        for line in f:
+            user, friend = line.rstrip("\n").split(" ")
+            if user in users:
+                users[user].append(friend)
             else:
-                groups[-1].append(friend)
-        else:
-            groups[-1].append(friend)
+                users[user] = [friend]
 
-for group in sorted(groups, key=lambda x: len(x)):
-    print(group)
+    return users
 
 
+def find_groups(users):
+    groups = []
+    for user in users:
+        added = False
+        # check if user already in groups list
+        for g in groups:
+            if user in g:
+                g.update(users[user])
+                added = True
+
+        # if not already in groups list, make a new group
+        if not added:
+            groups.append(set(user))
+            groups[-1].update(set([person for person in users[user]]))
+
+    # get lengths
+    group_lengths = [len(g) for g in groups]
+    return max(group_lengths)         
 
 
+def main():
+    users05 = read_in_file("./SoCS/friends05.txt")
+    max05 = find_groups(users05)
+
+    users10 = read_in_file("./SoCS/friends10.txt")
+    max10 = find_groups(users10)
+
+    print(f'''
+    max group size for friends05: {max05}
+    max group size for friends10: {max10}
+    ''')
+
+
+if __name__ == "__main__":
+    main()
